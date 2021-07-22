0cb03eb20ce5d1b8ce6bc461eeb121f3031a30c7
Harshit Vyas <harshitvyas@outlook.com>
2021-07-22 12:30:36 +0530

    ReadXML

---
+++ ReadXML/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ReadXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lt = new List<string>();
+            XDocument doc = XDocument.Load("xxe.xml"); // Or whatever
+            var allElements = doc.Descendants();
+            var matchingElements = doc.Descendants()
+                          .Where(x => x.Attribute("contentId") != null);
+            foreach(var x in matchingElements)
+            {
+                
+                lt.Add(x.FirstAttribute.Value);
+            }
+            
+            foreach(string s in lt)
+            {
+                Console.WriteLine(s);                
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
